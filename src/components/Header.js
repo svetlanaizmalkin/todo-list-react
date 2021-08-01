00833eb701c412576bead5ac0884595db627473c
Светлана Измалкина <svetlana_izmalkina@mail.ru>
2021-08-01 16:34:52 +0300

    media queries

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({ setTodos, deleteCheckedTodos }) => {
     return (
         <header className="header">
             <h1 className="header__h1">To do:</h1>
-            <button className="header__btn" onClick={deleteCheckedTodos}>Delete checked</button>
+            <button className="header__btn" onClick={deleteCheckedTodos}>Clear checked</button>
         </header>
     )
 }
